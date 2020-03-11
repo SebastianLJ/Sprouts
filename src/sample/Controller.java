cb23fc8bc5cecaf58d2bab121de349df8ceb9bd1
SebastianLJ <sebastian.lund.j@gmail.com>
2020-03-11 14:22:39 +0100

    added validateFile method to Controller and added new exception InvalidFileSyntax

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import Exceptions.InvalidFileSyntax;
+
+import java.io.*;
+
 public class Controller {
+
+    public static boolean validateFile(String fileName) throws Exception {
+        File file = new File(fileName);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = reader.readLine();
+        int lineNumber = 1;
+        if (!line.matches("\\d+")) {
+            throw new InvalidFileSyntax(lineNumber);
+        }
+        while ((line = reader.readLine()) != null) {
+            lineNumber++;
+            if (!line.matches("\\d+\\s\\d+")) {
+                throw new InvalidFileSyntax(lineNumber);
+            }
+        }
+        return true;
+    }
 }
