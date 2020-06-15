a1026eaed6f5a7043d73f986dd3226109c1d4cfb
s184186 <s184186@student.dtu.dk>
2020-06-15 11:11:50 +0200

    Removed another unecessary comment

--- src/Controller/SproutController.java
+++ src/Controller/SproutController.java
@@ -95,7 +95,6 @@ public class SproutController extends Controller {
     }
 
     public void updateSize(double width, double height) {
-        System.out.println("updateSize width, height: " + width + ", " + height);
         sproutModel.setWidth(width);
         sproutModel.setHeight(height);
     }
